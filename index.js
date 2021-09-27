f6491ebc924f2c3470480f4fbfe2c42a3dc0818d
Kheoh Yee Wei <oldjoy@protonmail.com>
2021-09-27 22:11:59 +0800

    wip

--- index.js
+++ index.js
@@ -1,40 +1,54 @@
-const downloadButton = document.getElementById("download");
-const playButton = document.getElementById("play");
+window.addEventListener("DOMContentLoaded", () => {
+  const downloadButton = document.getElementById("download");
+  const playButton = document.getElementById("play");
 
-const wavesurfer = WaveSurfer.create({
-  container: "#waveform",
-  waveColor: "violet",
-  height: 200,
-  progressColor: "purple",
-  normalize: true,
-  plugins: [
-    WaveSurfer.regions.create({
-      dragSelection: true,
-    }),
-  ],
-});
+  const wavesurfer = WaveSurfer.create({
+    container: "#waveform",
+    waveColor: "violet",
+    height: 200,
+    progressColor: "purple",
+    backend: "MediaElement",
+    normalize: true,
+    plugins: [
+      WaveSurfer.regions.create({
+        dragSelection: true,
+        deferInit: true,
+      }),
+      WaveSurferZoomToMousePlugin.create({
+        maxPxPerSec: 5000, // default 1000
+      }),
+    ],
+  });
 
-wavesurfer.on("region-updated", console.log);
+  wavesurfer.on("region-click", function (region, e) {
+    e.stopPropagation();
+    // Play on click, loop on shift click
+    e.shiftKey ? region.playLoop() : region.play();
+  });
 
-playButton.addEventListener("click", () => {
-  wavesurfer.play();
-});
+  wavesurfer.on("ready", () => wavesurfer.playPause());
 
-downloadButton.addEventListener("click", () => {
-  fetch("http://localhost:4000/haha")
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("HTTP error " + response.status);
-      }
-      return response.json();
-    })
-    .then((peaks) => {
-      console.log("loaded peaks! sample_rate: " + peaks.sample_rate);
+  playButton.addEventListener("click", () => {
+    wavesurfer.playPause();
+  });
 
-      // load peaks into wavesurfer.js
-      wavesurfer.load("http://localhost:4000/bamxPYj0O9M.mp3", peaks.data);
-    })
-    .catch((e) => {
-      console.error("error", e);
-    });
+  downloadButton.addEventListener("click", () => {
+    fetch("http://localhost:4000/haha")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("HTTP error " + response.status);
+        }
+        return response.json();
+      })
+      .then((peaks) => {
+        console.log("loaded peaks! sample_rate: " + peaks.sample_rate);
+
+        // load peaks into wavesurfer.js
+        wavesurfer.load("http://localhost:4000/bamxPYj0O9M.mp3", peaks.data);
+        wavesurfer.initPlugin("regions");
+      })
+      .catch((e) => {
+        console.error("error", e);
+      });
+  });
 });
---
+++ zoom.js
@@ -0,0 +1,91 @@
+export default class ZoomToMousePlugin {
+  static create(params) {
+    return {
+      name: "zoomToMousePlugin",
+      deferInit: params && params.deferInit ? params.deferInit : false,
+      params: params,
+      staticProps: {
+        setMaxPxPerSec(maxPxPerSec) {
+          this.zoomToMousePlugin.params.maxPxPerSec = maxPxPerSec;
+        },
+      },
+      instance: ZoomToMousePlugin,
+    };
+  }
+
+  constructor(params, ws) {
+    this.params = Object.assign(params, {
+      maxPxPerSec: 1000,
+    });
+
+    this.wavesurfer = ws;
+    this.mouseDuration = null;
+  }
+
+  zoomToMouse(pxPerSec) {
+    if (!pxPerSec) {
+      this.wavesurfer.params.minPxPerSec =
+        this.wavesurfer.defaultParams.minPxPerSec;
+      this.wavesurfer.params.scrollParent = false;
+    } else {
+      this.wavesurfer.params.minPxPerSec = pxPerSec;
+      this.wavesurfer.params.scrollParent = true;
+    }
+    this.wavesurfer.drawBuffer();
+    this.wavesurfer.drawer.progress(
+      this.wavesurfer.backend.getPlayedPercents()
+    );
+    this.wavesurfer.drawer.recenter(this.mouseDuration);
+    this.wavesurfer.fireEvent("zoom", pxPerSec);
+  }
+
+  _onWaveFormMouseWheelEvent(e) {
+    e.preventDefault();
+    const zoom = this.wavesurfer.params.minPxPerSec;
+    const maxZoom = this.params.maxPxPerSec;
+    var deltaY = 0;
+    if (e.deltaY) {
+      deltaY = e.deltaY;
+    } else if (e.wheelDelta) {
+      deltaY = -e.wheelDelta;
+    }
+    const zoomRatio = maxZoom / 10;
+    let zoomValue = zoom;
+    if (deltaY < 0) {
+      // zoom in
+      zoomValue = zoom + zoomRatio < maxZoom ? zoom + zoomRatio : maxZoom;
+    } else {
+      // zoom out
+      zoomValue = zoom - zoomRatio > 0 ? zoom - zoomRatio : 0;
+    }
+    this.zoomToMouse(zoomValue);
+  }
+
+  _onWaveFormMouseMove(e) {
+    const ev = this.wavesurfer.drawer.handleEvent(e);
+    this.mouseDuration = ev;
+  }
+
+  init() {
+    this.wavesurfer.container.addEventListener(
+      "wheel",
+      this._onWaveFormMouseWheelEvent.bind(this)
+    );
+    this.wavesurfer.container.addEventListener(
+      "mousemove",
+      this._onWaveFormMouseMove.bind(this)
+    );
+  }
+
+  destroy() {
+    this.wavesurfer.container.removeEventListener(
+      "wheel",
+      this._onWaveFormMouseWheelEvent.bind(this)
+    );
+
+    this.wavesurfer.container.removeEventListener(
+      "mousemove",
+      this._onWaveFormMouseMove.bind(this)
+    );
+  }
+}
