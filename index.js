c64cccd85773b6176e15f7da69f12afa78d555a0
Kheoh Yee Wei <oldjoy@protonmail.com>
2021-09-27 09:37:26 +0800

    wip

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
-const downloadButton = document.getElementById('download')
+const downloadButton = document.getElementById("download");
+const playButton = document.getElementById("play");
 
-downloadButton.addEventListener('click', () => {
+const wavesurfer = WaveSurfer.create({
+  container: "#waveform",
+  waveColor: "violet",
+  progressColor: "purple",
+  normalize: true,
+});
 
-    fetch('http://localhost:4000/haha')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error("HTTP error " + response.status);
-            }
-            return response.json();
-        })
-        .then(peaks => {
-            console.log('loaded peaks! sample_rate: ' + peaks.sample_rate);
+playButton.addEventListener("click", () => {
+  wavesurfer.play();
+});
 
-            var wavesurfer = WaveSurfer.create({
-                container: '#waveform',
-                waveColor: 'violet',
-                progressColor: 'purple',
-                normalize: true
-            });
-            // load peaks into wavesurfer.js
-            wavesurfer.load('small.mp3', peaks.data);
-        })
-        .catch((e) => {
-            console.error('error', e);
-        });
-})
+downloadButton.addEventListener("click", () => {
+  fetch("http://localhost:4000/haha")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("HTTP error " + response.status);
+      }
+      return response.json();
+    })
+    .then((peaks) => {
+      console.log("loaded peaks! sample_rate: " + peaks.sample_rate);
+
+      // load peaks into wavesurfer.js
+      wavesurfer.load("http://localhost:4000/bamxPYj0O9M.mp3", peaks.data);
+    })
+    .catch((e) => {
+      console.error("error", e);
+    });
+});
