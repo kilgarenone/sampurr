01d3e038fea94db478b8fba19ce371eb52b3ac01
Kheoh Yee Wei <oldjoy@protonmail.com>
2021-09-30 07:18:48 +0800

    draw region with canvas

--- index.js
+++ index.js
@@ -44,14 +44,34 @@ downloadButton.addEventListener("click", () => {
 });
 
 let isDown = false;
-let startX;
+let scrollStartX;
 let scrollLeft;
+
 let maybeDoubleClickDragging = false;
 let maybeDoubleClickDraggingTimeout;
+
+const canvas = document.createElement("canvas");
+canvas.id = "canvas";
+const { width, height, left, top } =
+  wavesurfer.container.getBoundingClientRect();
+canvas.width = width;
+canvas.height = height;
+wavesurfer.container.appendChild(canvas);
+
+var ctx = canvas.getContext("2d");
+ctx.strokeStyle = "blue";
+ctx.lineWidth = 3;
+var offsetX = left;
+var offsetY = top;
+var startX;
+var startY;
+let mouseX;
+let mouseY;
 
 wavesurfer.container.addEventListener("click", function (e) {
   maybeDoubleClickDragging = true;
 
+  // maybs00);
   // const rect = e.target.getBoundingClientRect();
   // const x = e.clientX - rect.left; // x position along the waveform
   // const percent = x / wavesurfer.container.firstChild.scrollWidth; // 0 to 1 range, 0 = start, 1 = end
@@ -63,13 +83,20 @@ wavesurfer.container.addEventListener("click", function (e) {
 });
 
 wavesurfer.container.addEventListener("mousedown", (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+
   if (maybeDoubleClickDragging) {
     clearTimeout(maybeDoubleClickDraggingTimeout);
+    // save the starting x/y of the rectangle
+    // startX = parseInt(e.clientX - offsetX);
+    // startY = parseInt(e.clientY - offsetY);
+
     return;
   }
 
   isDown = true;
-  startX = e.pageX - wavesurfer.container.firstChild.offsetLeft;
+  scrollStartX = e.pageX - wavesurfer.container.firstChild.offsetLeft;
   scrollLeft = wavesurfer.container.firstChild.scrollLeft;
 });
 
@@ -77,7 +104,10 @@ wavesurfer.container.addEventListener("mouseleave", () => {
   isDown = false;
 });
 
-wavesurfer.container.addEventListener("mouseup", (event) => {
+wavesurfer.container.addEventListener("mouseup", (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+
   maybeDoubleClickDraggingTimeout = setTimeout(() => {
     maybeDoubleClickDragging = false;
   }, 200);
@@ -86,10 +116,32 @@ wavesurfer.container.addEventListener("mouseup", (event) => {
 });
 
 wavesurfer.container.addEventListener("mousemove", (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+
+  if (maybeDoubleClickDragging) {
+    console.log("maybeDoubleClickDragging:", maybeDoubleClickDragging);
+    // get the current mouse position
+    // mouseX = parseInt(e.clientX - offsetX);
+    // mouseY = parseInt(e.clientY - offsetY);
+    // // calculate the rectangle width/height based
+    // // on starting vs current mouse position
+    // var width = mouseX - startX;
+    // var height = mouseY - startY;
+
+    // // clear the canvas
+    // ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // // draw a new rect from the start position
+    // // to the current mouse position
+    // ctx.strokeRect(startX, startY, width, height);
+
+    return;
+  }
+
   if (!isDown) return;
 
-  e.preventDefault();
   const x = e.pageX - wavesurfer.container.firstChild.offsetLeft;
-  const walk = (x - startX) * 3; //scroll-fast
+  const walk = (x - scrollStartX) * 3; //scroll-fast
   wavesurfer.container.firstChild.scrollLeft = scrollLeft - walk;
 });
