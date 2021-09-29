0e64c9222f54d9295b4e3d8bb9b586ecb81ac4c8
Kheoh Yee Wei <oldjoy@protonmail.com>
2021-09-29 19:46:22 +0800

    double click dragging

--- index.js
+++ index.js
@@ -17,24 +17,10 @@ const wavesurfer = WaveSurfer.create({
   plugins: [WaveSurfer.regions.create(), ZoomToMousePlugin.create()],
 });
 
-console.log("wavesurfer:", wavesurfer);
 wavesurfer.on("region-click", function (region, e) {
   e.stopPropagation();
   // Play on click, loop on shift click
   e.shiftKey ? region.playLoop() : region.play();
-});
-
-wavesurfer.container.addEventListener("dblclick", function (e) {
-  const rect = e.target.getBoundingClientRect();
-  const x = e.clientX - rect.left; // x position along the waveform
-  const percent = x / wavesurfer.container.firstChild.scrollWidth; // 0 to 1 range, 0 = start, 1 = end
-  const time = wavesurfer.getDuration() * percent;
-  console.log("time:", time);
-
-  wavesurfer.addRegion({
-    start: time,
-    end: time + 5,
-  });
 });
 
 playButton.addEventListener("click", () => {
@@ -50,36 +36,67 @@ downloadButton.addEventListener("click", () => {
       return response.json();
     })
     .then((peaks) => {
-      console.log("loaded peaks! sample_rate: " + peaks.sample_rate);
-
       // load peaks into wavesurfer.js
       wavesurfer.load("http://localhost:4000/bamxPYj0O9M.mp3", peaks.data);
     })
     .catch((e) => {
       console.error("error", e);
     });
+});
 
-  let isDown = false;
-  let startX;
-  let scrollLeft;
+let isDown = false;
+let startX;
+let scrollLeft;
+let startClickX;
+let maybeDoubleClickDragging = false;
+let timeout;
 
-  wavesurfer.container.addEventListener("mousedown", (e) => {
-    isDown = true;
-    startX = e.pageX - wavesurfer.container.firstChild.offsetLeft;
-    scrollLeft = wavesurfer.container.firstChild.scrollLeft;
-  });
-  wavesurfer.container.addEventListener("mouseleave", () => {
-    isDown = false;
-  });
-  wavesurfer.container.addEventListener("mouseup", () => {
-    isDown = false;
-  });
-  wavesurfer.container.addEventListener("mousemove", (e) => {
-    if (!isDown) return;
-    e.preventDefault();
-    const x = e.pageX - wavesurfer.container.firstChild.offsetLeft;
-    const walk = (x - startX) * 3; //scroll-fast
-    wavesurfer.container.firstChild.scrollLeft = scrollLeft - walk;
-    console.log(walk);
-  });
+wavesurfer.container.addEventListener("click", function (e) {
+  maybeDoubleClickDragging = true;
+
+  // const rect = e.target.getBoundingClientRect();
+  // const x = e.clientX - rect.left; // x position along the waveform
+  // const percent = x / wavesurfer.container.firstChild.scrollWidth; // 0 to 1 range, 0 = start, 1 = end
+  // const time = wavesurfer.getDuration() * percent;
+  // wavesurfer.addRegion({
+  //   start: time,
+  //   end: time + 5,
+  // });
+});
+
+wavesurfer.container.addEventListener("mousedown", (e) => {
+  if (maybeDoubleClickDragging) {
+    clearTimeout(timeout);
+    console.log("setup for doubleclick dragging");
+    return;
+  }
+
+  isDown = true;
+
+  startX = e.pageX - wavesurfer.container.firstChild.offsetLeft;
+  scrollLeft = wavesurfer.container.firstChild.scrollLeft;
+});
+
+wavesurfer.container.addEventListener("mouseleave", () => {
+  isDown = false;
+});
+
+wavesurfer.container.addEventListener("mouseup", (event) => {
+  timeout = setTimeout(function () {
+    console.log("inside settimepotu");
+    maybeDoubleClickDragging = false;
+  }, 200);
+  isDown = false;
+});
+
+wavesurfer.container.addEventListener("mousemove", (e) => {
+  if (!isDown) return;
+  console.log(
+    "maybeDoubleClickDragging inside mousemove",
+    maybeDoubleClickDragging
+  );
+  e.preventDefault();
+  const x = e.pageX - wavesurfer.container.firstChild.offsetLeft;
+  const walk = (x - startX) * 3; //scroll-fast
+  wavesurfer.container.firstChild.scrollLeft = scrollLeft - walk;
 });
