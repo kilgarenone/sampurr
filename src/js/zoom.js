fee0333569bcd936217815b47771d64c3847a20a
yeewei <yeewei@setel.com>
2023-11-26 16:48:57 +0900

    wip upgarde wavesurfer v7

--- src/js/zoom.js
+++ src/js/zoom.js
@@ -1,85 +1,86 @@
-export class ZoomToMousePlugin {
-  static create(params) {
-    return {
-      name: "zoomToMousePlugin",
-      deferInit: params && params.deferInit ? params.deferInit : false,
-      params: params,
-      // staticProps: {
-      //   setMaxPxPerSec(maxPxPerSec) {
-      //     this.zoomToMousePlugin.params.maxPxPerSec = maxPxPerSec;
-      //   },
-      // },
-      instance: ZoomToMousePlugin,
-    };
+import BasePlugin from "wavesurfer.js/dist/base-plugin"
+import { state } from "../state"
+import { BASE_URL } from "../const"
+
+export class ZoomToMousePlugin extends BasePlugin {
+  static create() {
+    return new ZoomToMousePlugin()
   }
 
-  constructor(params, ws) {
+  constructor() {
+    super()
+
     this.params = {
       maxPxPerSec: 150,
-    };
-    this.currentZoom = 0;
-    this.wavesurfer = ws;
-    this.mouseDuration = null;
+    }
+    this.currentZoom = 0
+    // this.wavesurfer = null
+    this.mouseDuration = null
+    this.container = document.getElementById("waveform")
   }
 
   zoomToMouse(pxPerSec) {
-    if (!pxPerSec) {
-      this.wavesurfer.params.minPxPerSec =
-        this.wavesurfer.defaultParams.minPxPerSec;
-      this.wavesurfer.params.scrollParent = false;
-    } else {
-      this.wavesurfer.params.minPxPerSec = pxPerSec;
-      this.wavesurfer.params.scrollParent = true;
-    }
-    this.wavesurfer.drawBuffer();
-    this.wavesurfer.drawer.progress(
-      this.wavesurfer.backend.getPlayedPercents()
-    );
-    this.wavesurfer.drawer.recenter(this.mouseDuration);
-    this.wavesurfer.fireEvent("zoom", pxPerSec);
+    // if (!pxPerSec) {
+    //   this.wavesurfer.params.minPxPerSec =
+    //     this.wavesurfer.defaultParams.minPxPerSec
+    //   this.wavesurfer.params.scrollParent = false
+    // } else {
+    //   this.wavesurfer.params.minPxPerSec = pxPerSec
+    //   this.wavesurfer.params.scrollParent = true
+    // }
+    // replace this with wavesurfer.current.load(audioUrl, peaks)
+    // this.wavesurfer.drawBuffer()
+    // console.log(state)
+    // this.wavesurfer.load(`${BASE_URL}/${state.mediaID}.wav`, state.peaks)
+
+    this.wavesurfer.zoom(pxPerSec)
+    this.wavesurfer.seekTo(
+      this.wavesurfer.getCurrentTime() / this.wavesurfer.getDuration()
+    )
+    // this.wavesurfer.drawer.recenter(this.mouseDuration)
   }
 
   _onWaveFormMouseWheelEvent(event) {
-    event.preventDefault();
+    event.preventDefault()
 
-    const maxZoom = this.params.maxPxPerSec;
+    const maxZoom = this.params.maxPxPerSec
 
     if (event.wheelDelta > 0) {
       // zoom in
-      this.currentZoom = Math.min(this.currentZoom + 10, maxZoom);
+      this.currentZoom = Math.min(this.currentZoom + 10, maxZoom)
     } else {
       // zoom out
-      this.currentZoom = Math.max(this.currentZoom - 10, 0);
+      this.currentZoom = Math.max(this.currentZoom - 10, 0)
     }
 
-    this.zoomToMouse(this.currentZoom);
+    this.zoomToMouse(this.currentZoom)
   }
 
   _onWaveFormMouseMove(e) {
-    const ev = this.wavesurfer.drawer.handleEvent(e);
-    this.mouseDuration = ev;
+    // const ev = this.wavesurfer.drawer.handleEvent(e)
+    // this.mouseDuration = ev
   }
 
-  init() {
-    this.wavesurfer.container.addEventListener(
+  onInit() {
+    this.container.addEventListener(
       "wheel",
       this._onWaveFormMouseWheelEvent.bind(this)
-    );
-    this.wavesurfer.container.addEventListener(
+    )
+    this.container.addEventListener(
       "mousemove",
       this._onWaveFormMouseMove.bind(this)
-    );
+    )
   }
 
   destroy() {
-    this.wavesurfer.container.removeEventListener(
+    this.container.removeEventListener(
       "wheel",
       this._onWaveFormMouseWheelEvent.bind(this)
-    );
+    )
 
-    this.wavesurfer.container.removeEventListener(
+    this.container.removeEventListener(
       "mousemove",
       this._onWaveFormMouseMove.bind(this)
-    );
+    )
   }
 }
